b302058c8e4afe5a6a8788e7da4ab3899909fc1a
zhangmaosheng <zhang.ms@foxmail.com>
2018-08-21 23:22:20 +0800

    first commit

---
+++ noauth/install-etcd-service.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function create_serviceFile() {
+    mkdir -p /var/lib/etcd/
+    serviceFile="/usr/lib/systemd/system/etcd.service"
+    str="[Unit] \n
+         Description=Extcd Server\n
+         After=network.target\n
+         After=network-online.target\n
+         Wants=network-online.target\n\n[Service]\n
+         Type=notify\n
+         WorkingDirectory=/var/lib/etcd/\n
+         EnvironmentFile=-/etc/etcd/etcd.conf\n
+         ExecStart=/usr/local/bin/etcd ${ETCD_OPTIONS}\n
+         Restart=on-failure\n
+         RestartSec=5\n
+         LimitNOFILE=65536\n\n[Install]\n
+         WantedBy=multi-user.target\n"
+    echo -e $str > $serviceFile
+}
+
+function createConfigFile() {
+    mkdir /etc/etcd/
+    echo "" > /etc/etcd/etcd.conf
+}
+
+create_serviceFile
+createConfigFile
