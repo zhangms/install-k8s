6f202b600f10ca07edc927efccb62301a9aa2a5c
zhangmaosheng <zhang.ms@foxmail.com>
2018-08-22 13:58:39 +0800

    install-kubelet.sh

---
+++ noauth/install-kube-proxy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+kube_master_ip=$1
+kube_master_port=$2
+
+serviceName=kube-proxy
+
+function create_serviceFile() {
+    str="[Unit]\n
+         Description=Kubernetes Kube-Proxy Server\n
+         After=network.target\n\n
+         [Service]\n
+         EnvironmentFile=-/etc/kubernetes/config\n
+         EnvironmentFile=-/etc/kubernetes/proxy\n
+         ExecStart=/usr/local/bin/kube-proxy \$KUBE_MASTER \$KUBE_BASE_OPTIONS \$KUBE_PROXY_OPTIONS \n
+         Restart=on-failure\n
+         LimitNOFILE=65536\n\n
+         [Install]\n
+         WantedBy=multi-user.target\n"
+    echo -e $str > /usr/lib/systemd/system/${serviceName}.service
+}
+
+function createBaseConfig() {
+
+    mkdir /etc/kubernetes/
+    mkdir /var/log/kubernetes/
+
+    base_config='KUBE_BASE_OPTIONS="'
+    base_config+='--logtostderr=true --log-dir=/var/log/kubernetes --v=2'
+    base_config+='"\n'
+    base_config+='KUBE_MASTER="--master=http://'
+    base_config+=$kube_master_ip:$kube_master_port
+    base_config+='"\n'
+
+    echo -e $base_config > /etc/kubernetes/config
+}
+
+
+function createServiceConfig() {
+    mkdir /etc/kubernetes/
+    base_config='KUBE_PROXY_OPTIONS="'
+    base_config+=''
+    base_config+='"\n'
+    echo -e $base_config > /etc/kubernetes/proxy
+}
+
+function createConfigFile() {
+    createBaseConfig
+    createServiceConfig
+}
+
+create_serviceFile
+createConfigFile
+systemctl daemon-reload
+systemctl disable ${serviceName}
+systemctl enable ${serviceName}
+systemctl restart ${serviceName}
+systemctl status ${serviceName}
+
